9c84d2a403ed8389e6e6204588ae8f3ac43e26cd
ernestoj <ernestoj@ifi.uio.no>
2018-07-26 17:45:10 +0100

    Added KR

---
+++ src/main/java/uk/turing/aida/kb/KnowledgeResource.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright 2018 by The Alan Turing Institute
+ * 
+ *******************************************************************************/
+package uk.turing.aida.kb;
+
+/**
+ *
+ * @author ernesto
+ * Created on 26 Jul 2018
+ *
+ */
+public class KnowledgeResource {
+	
+	//Composed by an ontology and possibly data
+	//Data accessible/avilable via look-up (i.e. REST APIs), sparql end-points, as RDF.
+	//From DBpedia we will combine look-up and spqrql end-point to extract a "relevant" fragment to the given case.
+
+}
